431ea7a29912f0ef2b7617f4a9a601ffd47c31cf
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-15 15:04:28 +0100

    Hello world test for fixing 500 error

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ def post_actions(data):
 
 @app.route("/", methods=['GET', 'POST'])
 def indx():
-    if request.method == 'POST':
-        if request.data:
-            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-            rsp = post_actions(rcv_data)
-            if rsp:
-                return rsp
-            else:
-                return '200'
-        else:
-            return '404'
+    return "Hello World"
+    #if request.method == 'POST':
+    #    if request.data:
+    #        rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+    #        rsp = post_actions(rcv_data)
+    #        if rsp:
+    #            return rsp
+    #        else:
+    #            return '200'
+    #    else:
+    #        return '404'
