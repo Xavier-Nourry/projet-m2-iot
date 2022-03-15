bad85ee2e502e0b04351f2633ffdcf5f4ce898e1
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-15 15:11:34 +0100

    adding default text for fixing 500 error

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ import json
 app = Flask(__name__)
 
 def post_actions(data):
-    print(data)
     return data
 
 @app.route("/", methods=['GET', 'POST'])
 def indx():
-    return "Hello World"
-    #if request.method == 'POST':
-    #    if request.data:
-    #        rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-    #        rsp = post_actions(rcv_data)
-    #        if rsp:
-    #            return rsp
-    #        else:
-    #            return '200'
-    #    else:
-    #        return '404'
+    #return "Hello World"
+    if request.method == 'POST':
+        if request.data:
+            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+            rsp = post_actions(rcv_data)
+            if rsp:
+                return rsp
+            else:
+                return '200'
+        else:
+            return 'Projet M2 INFO - Pilulier Intelligent'
