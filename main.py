e7a17dd14292e5479a7c8a9f292ca9e9305f04a0
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-15 15:59:27 +0100

    wip

--- main.py
+++ main.py
@@ -9,30 +9,32 @@ data = []
 @app.route("/notify-uplink", methods=['GET', 'POST'])
 def notify_uplink():
     global data
-    if request.method == 'POST':
-        if request.data:
-                rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-                if rcv_data: 
-                    data.insert(0, rcv_data)
-                if len(data) != 0:
-                    return data[0]
-                else:
-                    return '200'
-        else:
-            return 'No data passed with the request'
-    elif request.method == 'GET':
-        return 'Request passed by GET method'
-    else:
-        return 'Not a valid request'
+    if request.data:
+        rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+        if rcv_data: 
+            data.insert(0, rcv_data)
+    #if request.method == 'POST':
+    #    if request.data:
+    #            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+    #            if rcv_data: 
+    #                data.insert(0, rcv_data)
+    #            else:
+    #                return '200'
+    #    else:
+    #        return 'No data passed with the request'
+    #elif request.method == 'GET':
+    #    return 'Request passed by GET method'
+    #else:
+    #    return 'Not a valid request'
 
 
 @app.route("/")
 def indx():
     global data
-    to_display = "Projet M2 IOT - Pilulier Intelligent</br></br>"
+    to_display = "<h2>Projet M2 IOT - Pilulier Intelligent</h2></br></br>"
     if len(data) != 0:
         for item in data:
-            to_display += item + "</br>"
+            to_display += str(item) + "</br>"
     return to_display
     #if request.method:
     #    if request.method == 'POST':
