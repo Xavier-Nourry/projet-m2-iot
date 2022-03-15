921da7bbcecb5adf45d97453572539aa9b455f45
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-15 15:33:39 +0100

    imported Flask.request and added new route for requests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import request
 import json
 
@@ -7,19 +7,37 @@ app = Flask(__name__)
 def post_actions(data):
     return data
 
-@app.route("/", methods=['GET', 'POST'])
-def indx():
-    #return "Hello World"
-    if request.method:
-        if request.method == 'POST':
-            if request.data:
+@app.route("/notify-uplink", methods=['GET', 'POST'])
+def notify_uplink():
+    if request.method == 'POST':
+        if request.data:
                 rcv_data = json.loads(request.data.decode(encoding='utf-8'))
                 rsp = post_actions(rcv_data)
                 if rsp:
                     return rsp
                 else:
                     return '200'
-            else:
-                return 'No data passed with the request'
+        else:
+            return 'No data passed with the request'
+    elif request.method == 'GET':
+        return 'Request passed by GET method'
     else:
-        return 'Projet M2 INFO - Pilulier Intelligent'
+        return 'Not a valid request'
+
+
+@app.route("/")
+def indx():
+    return "Projet M2 IOT - Pilulier Intelligent"
+    #if request.method:
+    #    if request.method == 'POST':
+    #        if request.data:
+    #            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+    #            rsp = post_actions(rcv_data)
+    #            if rsp:
+    #                return rsp
+    #            else:
+    #                return '200'
+    #        else:
+    #            return 'No data passed with the request'
+    #else:
+    #    return redirect(url_for('notify-uplink'))
