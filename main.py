5e71d78662122e702c501245242cb9072ce1386f
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-15 15:15:41 +0100

    testing if request method exist for fixing 500 error

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ def post_actions(data):
 @app.route("/", methods=['GET', 'POST'])
 def indx():
     #return "Hello World"
-    if request.method == 'POST':
-        if request.data:
-            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-            rsp = post_actions(rcv_data)
-            if rsp:
-                return rsp
+    if request.method != None:
+        if request.method == 'POST':
+            if request.data:
+                rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+                rsp = post_actions(rcv_data)
+                if rsp:
+                    return rsp
+                else:
+                    return '200'
             else:
-                return '200'
-        else:
-            return 'Projet M2 INFO - Pilulier Intelligent'
+                return 'No data passed with the request'
+    else:
+        return 'Projet M2 INFO - Pilulier Intelligent'
