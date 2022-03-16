f73d3198140c2a171c45df01a42a9b9622a680f1
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-16 08:59:27 +0100

    code refactor

--- main.py
+++ main.py
@@ -2,19 +2,12 @@ from flask import Flask
 import http.client, urllib
 import requests
 
+
 app = Flask(__name__)
 
 
 @app.route("/notify-uplink", methods=['GET', 'POST'])
 def notify_uplink():
-    #conn = http.client.HTTPSConnection("api.pushover.net:443")
-    #conn.request("POST", "/1/messages.json",
-    #urllib.parse.urlencode({
-    #    "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
-    #    "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
-    #    "message": "Alerte SmartPillBox : Roger n'a pas pris ses cachets",
-    #}), { "Content-type": "application/x-www-form-urlencoded" })
-    
     r = requests.post("https://api.pushover.net/1/messages.json", data = {
         "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
         "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
@@ -23,7 +16,14 @@ def notify_uplink():
     files = {
         "attachment": ("logo.jpg", open("resources/images/logo.jpg", "rb"), "image/jpeg")
     })
-
+    #conn = http.client.HTTPSConnection("api.pushover.net:443")
+    #conn.request("POST", "/1/messages.json",
+    #urllib.parse.urlencode({
+    #    "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
+    #    "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
+    #    "message": "Alerte SmartPillBox : Roger n'a pas pris ses cachets",
+    #}), { "Content-type": "application/x-www-form-urlencoded" })
+    
 
 @app.route("/")
 def indx():
