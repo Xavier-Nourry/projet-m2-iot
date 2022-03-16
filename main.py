accceb7ec086802db3474798be6f2e1a4ee589f3
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-16 08:55:29 +0100

    push notification attached image test

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from flask import Flask
 import http.client, urllib
-
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/notify-uplink", methods=['GET', 'POST'])
 def notify_uplink():
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-    urllib.parse.urlencode({
+    #conn = http.client.HTTPSConnection("api.pushover.net:443")
+    #conn.request("POST", "/1/messages.json",
+    #urllib.parse.urlencode({
+    #    "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
+    #    "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
+    #    "message": "Alerte SmartPillBox : Roger n'a pas pris ses cachets",
+    #}), { "Content-type": "application/x-www-form-urlencoded" })
+    
+    r = requests.post("https://api.pushover.net/1/messages.json", data = {
         "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
         "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
-        "message": "Alerte SmartPillBox : Roger n'a pas pris ses cachets",
-    }), { "Content-type": "application/x-www-form-urlencoded" })
-    #conn.getresponse()
+        "message": "Alerte SmartPills : Roger n'a pas pris ses cachets"
+    },
+    files = {
+        "attachment": ("logo.jpg", open("resources/images/logo.jpg", "rb"), "image/jpeg")
+    })
 
 
 @app.route("/")
