62df93f64f708d34988274c1d514329327f9ede5
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2022-03-18 15:13:47 +0100

    arduino code update

--- arduino/main.ino
+++ arduino/main.ino
@@ -11,19 +11,20 @@ const char *DEV_ADDR = "260B9F2A";
 const char *NWK_SKEY = "C927B91EDAFD2FFC7EB2DAB162F90BA1";
 const char *APP_SKEY = "815DC9CB8FCE1223203EE74DCDFC97E7";
 
-const int ENGINE_PIN = 12;
-const int SWITCH_PIN = 4;
-const int BUTTON_PIN = 8;
-const int LED_PIN = 7;
-const int BUZZER_PIN = 2;
-const int BUZZER_SWITCH_PIN = 6;
-const int BIPS_NB = 4;
+const int ENGINE_PIN = 4;
+const int SWITCH_PIN = 8;
+const int CONTACT_PIN = 12;
+const int CONTACT_ALIM_PIN = 13;
+const int LED_PIN = 6;
+const int BUZZER_PIN = 5;
+const int BUZZER_SWITCH_PIN = 7;
+const int BIPS_NB = 2;
 const int BIPS_FREQ = 250; // milliseconds
 const int NB_ROT = 7;
 const int ROT_ANGLE = (int)(155 / (float) 8);
-const int TIME_DISTRIB = 2500; // milliseconds
-const int TIME_TO_WAIT = 5; // seconds
-const char* MSG_TO_SEND = "Emergency";
+const int TIME_DISTRIB = 3000; // milliseconds
+const int TIME_TO_WAIT = 10; // seconds
+const char* MSG_TO_SEND = "ALERT";
 
 
 // Définition des variables
@@ -49,12 +50,6 @@ void take_pills_alert(){
   delay(2000);
 }
 
-// Lance un appel d'urgence via LORA (lié à une application IP)
-void emergency_call(){
-  debugSerial.print("Sending msg...");
-  ttn.sendBytes(MSG_TO_SEND, sizeof(MSG_TO_SEND));
-  debugSerial.print("Message sent");    
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -67,16 +62,19 @@ void setup() {
   engine.attach(ENGINE_PIN);
   pinMode(ENGINE_PIN, OUTPUT);
   pinMode(SWITCH_PIN, INPUT);  
+  pinMode(CONTACT_PIN, INPUT);
+  pinMode(CONTACT_ALIM_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUZZER_SWITCH_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
+  digitalWrite(CONTACT_ALIM_PIN, HIGH);
   engine.write(actual_angle);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(SWITCH_PIN) == HIGH){
+  if(digitalRead(SWITCH_PIN) == HIGH){ // Si switch système sur on
     if(rot_cpt >= NB_ROT){
       actual_angle = 155;
       rot_cpt = 0;
@@ -84,17 +82,20 @@ void loop() {
         actual_angle -= ROT_ANGLE;
         rot_cpt++;
       }
-    
     engine.write(actual_angle);
+    
     current_time = now();
     start_time = current_time;
-    while(digitalRead(BUTTON_PIN) != LOW){ // TODO : voir si nécessaire de faire des actions en parallèle
+    while((digitalRead(CONTACT_PIN) == HIGH) && (digitalRead(SWITCH_PIN) == HIGH)){
       current_time = now();
-      take_pills_alert();
+      take_pills_alert(); // Alerte Roger pour qu'il prenne ses cachets
       if(current_time >= start_time + TIME_TO_WAIT){
-        start_time = now();
-        emergency_call();
+        start_time = now(); // Remise à 0 du timer pour relancer une alerte quand le temps s'est ré-écoulé depuis la 1ere alerte
+        ttn.sendBytes(MSG_TO_SEND, sizeof(MSG_TO_SEND)); // Lance un appel d'urgence via LORA (lié à une application IP)
       }
+    }
+    while(digitalRead(CONTACT_PIN) == LOW){
+      delay(100);
     }
     delay(TIME_DISTRIB);
   }
