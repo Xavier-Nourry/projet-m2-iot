71faca46abfe9c82c3f30c00bffd5f2e4a04f7b4
Xavier-Nourry <75121454+Xavier-Nourry@users.noreply.github.com>
2022-03-18 15:19:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route("/notify-uplink", methods=['GET', 'POST'])
 def notify_uplink():
     r = requests.post("https://api.pushover.net/1/messages.json", data = {
-        "token": "afneydpm7t3oxqg64o4vgi7kchjrzk",
-        "user": "uspcin8whkjxgveh9oy2ursk24w3vr",
+        "token": "", # Récupérer une clé API PushOver
+        "user": "", # Récupérer un user ID PushOver
         "message": "Alerte ! Roger Martinez n'a pas pris ses cachets !\n06 00 00 00 00\n185 Rue de la paix, 73370 Le Bourget-du-lac"
     },
     files = {
